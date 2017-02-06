f9615fb8863ee4c6e46d3981f06732ee1b7e2ab7
askrynnikov <5101233@gmail.com>
2017-02-06 10:45:07 +0300

    add test for model Question, Answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it do
+    should validate_presence_of(:body)
+  end
+
+  it do
+    should belong_to(:question).
+      with_foreign_key('question_id')
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it { is_expected.to validate_presence_of(:title) }
+  it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
+
+  it { should have_many(:answers) }
 
   # it 'validates presence of title' do
   #   expect(Question.new(body: '123')).to_not be_valid
