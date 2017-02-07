721c4358bceb296eea52b165d91bf4994a448f72
askrynnikov <5101233@gmail.com>
2017-02-07 19:25:15 +0300

    remove require 'rails_helper' in tests
    по совету наставника в .rspec заменил
    --require spec_helper на --require rails_helper
    теперь в тесты *_spec.rb можно не добавлять require 'rails_helper'

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Answer, type: :model do
   describe 'associations' do
     it do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-require 'rails_helper'
-
 RSpec.describe Question, type: :model do
   describe 'associations' do
     it do
