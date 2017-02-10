c8e004cd1dec15b161a38665cc4ec43d9aff3f6c
askrynnikov <5101233@gmail.com>
2017-02-10 19:30:02 +0300

    use let in Spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+
     before do
-      @questions = create_list(:question, 2)
       get :index
     end
 
     it 'populates an array of all questions' do
-      expect(assigns(:questions)).to match_array(@questions)
+      expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'render index view' do
