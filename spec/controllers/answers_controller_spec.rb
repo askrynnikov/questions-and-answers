600d63fa7a2ccef09942d49654a77fdb6ca2ccf2
askrynnikov <5101233@gmail.com>
2017-02-12 06:47:29 +0300

    add test new for answers_controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  # let(:answer) { create(:answer) }
 
+  describe 'GET #new' do
+    # before { get :new, {} }
+
+    it 'assigns a new Answer to @answer' do
+      answer1 = FactoryGirl.create(:answer)
+
+      get :new, params: { :question_id => answer1.question_id }
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    # it 'render new view' do
+    #   expect(response).to render_template :new
+    # end
+  end
 end
