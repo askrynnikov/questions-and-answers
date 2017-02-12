f066e72431bbf21fd59a18c463a9b9c516a0f98a
askrynnikov <5101233@gmail.com>
2017-02-12 07:14:15 +0300

    refactoring test new for answers_controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,22 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  # let(:answer) { create(:answer) }
+  let(:answer) { create(:answer) }
 
   describe 'GET #new' do
-    # before { get :new, {} }
+    before { get :new, params: { :question_id => answer.question_id } }
 
     it 'assigns a new Answer to @answer' do
-      answer1 = FactoryGirl.create(:answer)
-
-      get :new, params: { :question_id => answer1.question_id }
+      # answer1 = FactoryGirl.create(:answer)
+      # get :new, params: { :question_id => answer1.question_id }
       expect(assigns(:answer)).to be_a_new(Answer)
     end
 
     it 'render new view' do
-      answer1 = FactoryGirl.create(:answer)
-
-      get :new, params: { :question_id => answer1.question_id }
       expect(response).to render_template :new
     end
   end
