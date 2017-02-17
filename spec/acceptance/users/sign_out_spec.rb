176fcf7da7ee542a18ca8759cb3828aaffe2623c
askrynnikov <5101233@gmail.com>
2017-02-17 18:05:07 +0300

    Done acceptance test for story "Authenticated user can sign out"
     - Пользователь может выйти из системы

---
+++ spec/acceptance/users/sign_out_spec.rb
@@ -0,0 +1,15 @@
+RSpec.feature 'User sign out', %q{
+  in order to end session
+  As an user
+  I want to be able to sign out
+} do
+
+  given(:user) {create(:user)}
+
+  scenario 'Authenticated user sign out' do
+    sign_in(user)
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
