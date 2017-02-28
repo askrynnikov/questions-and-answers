a00fe085a1b7378a5def9920baafc23d38eb2dba
askrynnikov <5101233@gmail.com>
2017-02-28 10:46:52 +0300

    determine the exact routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
 
   resources :questions do
-    resources :answers, shallow: true  do
-      # patch 'set_best', on: :member
-    end
-
+    resources :answers, only: [:create]
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :answers, only: [:update, :destroy]  do
+    patch 'mark_best', on: :member
+  end
 end
