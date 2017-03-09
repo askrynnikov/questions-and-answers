a4919ae9951b7062ca3a2b350c9408c0edc57c90
askrynnikov <5101233@gmail.com>
2017-03-09 20:08:40 +0300

    add consern Attachable

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,7 @@
 class Question < ApplicationRecord
   include HasUser
+  include Attachable
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachable
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   validates :title, :body, :user_id, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: proc { |attrib| attrib['file'].nil? }
 end
