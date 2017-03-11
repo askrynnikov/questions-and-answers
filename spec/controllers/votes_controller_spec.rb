8a09e8abb01379163f5c6d5ba45bcdcaf844911a
askrynnikov <5101233@gmail.com>
2017-03-11 07:40:56 +0300

    user can cancel Vote

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -48,22 +48,37 @@ RSpec.describe VotesController, type: :controller do
           it 'votable type missing' do
             post :create, params: votable_type_missing
             data = JSON.parse(response.body)
-            expect(response).to have_http_status :unprocessable_entity
-            expect(data['error']).to eq 'Error save'
+            expect(response).to have_http_status :bad_request
+            expect(data['error']).to eq 'Error'
             expect(data['error_message']).to eq 'Not the correct vote data!'
           end
 
           it 'invalid votable type' do
             post :create, params: invalid_votable_type
             data = JSON.parse(response.body)
-            expect(response).to have_http_status :unprocessable_entity
-            expect(data['error']).to eq 'Error save'
+            expect(response).to have_http_status :bad_request
+            expect(data['error']).to eq 'Error'
             expect(data['error_message']).to eq 'Not the correct vote data!'
           end
         end
       end
 
-      context 'User is author votable'
+      context 'User is author votable' do
+        let!(:vote_params) { {question_id: question, votable_type: question.class.name, rating: 'up', format: :json} }
+        before { sign_in question.user }
+
+        it 'vote not stored in the database' do
+          expect { post :create, params: vote_params }.to_not change(Vote, :count)
+        end
+
+        it 'render error' do
+          post :create, params: vote_params
+          data = JSON.parse(response.body)
+          expect(response).to have_http_status :forbidden
+          expect(data['error']).to eq 'Error save'
+          expect(data['error_message']).to eq 'You can not vote'
+        end
+      end
     end
 
     context 'Non-authenticated user' do
@@ -71,6 +86,58 @@ RSpec.describe VotesController, type: :controller do
         expect do
           post :create, params: vote_params
         end.to_not change(Vote, :count)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:user) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:question) { create(:question, user: user2) }
+    let!(:vote) { create(:vote, votable: question, user: user) }
+
+    context 'Authenticated user' do
+      before { sign_in vote.user }
+      context 'author vote' do
+        it 'delete vote' do
+          expect { delete :destroy, params: {id: vote.id, format: :json} }.to change(Vote, :count).by(-1)
+        end
+
+        it 'render success' do
+          delete :destroy, params: {id: vote.id, format: :json}
+          question.reload
+          data = JSON.parse(response.body)
+          expect(response).to have_http_status :success
+
+          expect(data['id']).to eq vote.id
+          expect(data['votable_rating']).to eq question.rating
+          expect(data['votable_type']).to eq question.class.name.underscore
+          expect(data['votable_id']).to eq question.id
+          expect(data['action']).to eq 'delete'
+          expect(data['message']).to eq 'Your vote removed!'
+        end
+      end
+
+      context 'not author vote' do
+        before { sign_in question.user }
+
+        it 'delete vote' do
+          expect { delete :destroy, params: {id: vote.id, format: :json} }.to_not change(Vote, :count)
+        end
+
+        it 'render error' do
+          delete :destroy, params: {id: vote.id, format: :json}
+          data = JSON.parse(response.body)
+          expect(response).to have_http_status :forbidden
+          expect(data['error']).to eq 'Error remove'
+          expect(data['error_message']).to eq 'You can not remove an vote!'
+        end
+      end
+    end
+
+    context 'Non-authenticated user' do
+      it 'delete vote' do
+        expect { delete :destroy, params: {id: vote.id, format: :json} }.to_not change(Vote, :count)
       end
     end
   end
