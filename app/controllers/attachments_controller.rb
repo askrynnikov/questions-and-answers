062dce55062064d2cc5fd027bd9ba4385e386401
askrynnikov <5101233@gmail.com>
2017-03-20 17:11:38 +0300

    32 AttachmentsController with responders

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,15 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_attachment, only: [:destroy]
+
+  respond_to :js
 
   def destroy
-    @attachment = Attachment.find(params[:id])
     @attachment.destroy if current_user.author_of?(@attachment.attachable)
+    respond_with(@attachment)
+  end
+
+  def set_attachment
+    @attachment = Attachment.find(params[:id])
   end
 end
