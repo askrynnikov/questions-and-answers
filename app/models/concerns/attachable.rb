cfffef6739d3a6085f1216ec683d8acde060076e
askrynnikov <5101233@gmail.com>
2017-04-01 16:51:08 +0300

    refactoring, use method concerm

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -1,5 +1,4 @@
-module Attachable
-  extend ActiveSupport::Concern
+concern :Attachable do
   included do
     has_many :attachments, as: :attachable, dependent: :destroy
     accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
