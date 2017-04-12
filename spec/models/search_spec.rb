f15e9962f26ce5a2e9e968fc3d120af6bc88626c
askrynnikov <5101233@gmail.com>
2017-04-12 10:36:49 +0300

    39 Search model

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe Search do
+  describe '.do' do
+    let(:params) { { q: 'search text', scopes: %w(question answer comment user) } }
+
+    it 'escapes query' do
+      expect(ThinkingSphinx::Query).to receive(:escape).with(params[:q])
+      Search.do(params)
+    end
+
+    context 'scopes' do
+      context 'scope all' do
+        it 'set scope all' do
+          params[:scopes] += ['all']
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1)
+          Search.do(params)
+        end
+
+        it 'default scope set all' do
+          params[:scopes] = nil
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1)
+          Search.do(params)
+        end
+      end
+
+      it 'classify scope' do
+        classes = params[:scopes].map { |scope| scope.classify.constantize }
+        expect(ThinkingSphinx).to receive(:search).with(params[:q], classes: classes, page: 1)
+        Search.do(params)
+      end
+
+      context 'pages' do
+        let(:params) { { q: 'search text', scopes: ['all'] } }
+
+        it 'default page 1' do
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1)
+          Search.do(params)
+        end
+
+        it 'set page' do
+          params[:page] = rand(1..10)
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: params[:page])
+          Search.do(params)
+        end
+      end
+    end
+  end
+end
