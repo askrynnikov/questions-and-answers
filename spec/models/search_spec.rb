bde8ab2a700b79c1acc662b55703f640517d32a3
askrynnikov <5101233@gmail.com>
2017-04-12 17:29:49 +0300

    39 pagination search results

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Search do
   describe '.do' do
-    let(:params) { { q: 'search text', scopes: %w(question answer comment user) } }
+    let(:params) { { q: 'search text', scopes: %w(question answer comment user)} }
 
     it 'escapes query' do
       expect(ThinkingSphinx::Query).to receive(:escape).with(params[:q])
@@ -11,20 +11,20 @@ RSpec.describe Search do
       context 'scope all' do
         it 'set scope all' do
           params[:scopes] += ['all']
-          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1)
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1, per_page: 5)
           Search.do(params)
         end
 
         it 'default scope set all' do
           params[:scopes] = nil
-          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1)
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1, per_page: 5)
           Search.do(params)
         end
       end
 
       it 'classify scope' do
         classes = params[:scopes].map { |scope| scope.classify.constantize }
-        expect(ThinkingSphinx).to receive(:search).with(params[:q], classes: classes, page: 1)
+        expect(ThinkingSphinx).to receive(:search).with(params[:q], classes: classes, page: 1, per_page: 5)
         Search.do(params)
       end
 
@@ -32,13 +32,13 @@ RSpec.describe Search do
         let(:params) { { q: 'search text', scopes: ['all'] } }
 
         it 'default page 1' do
-          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1)
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: 1, per_page: 5)
           Search.do(params)
         end
 
         it 'set page' do
           params[:page] = rand(1..10)
-          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: params[:page])
+          expect(ThinkingSphinx).to receive(:search).with(params[:q], page: params[:page], per_page: 5)
           Search.do(params)
         end
       end
